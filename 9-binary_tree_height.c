0fca7fb8aa0884d7eafdaa7433a96b1fa7525135
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 09:41:12 +0000

    fix

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -8,16 +8,13 @@
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
-	size_t left_height, right_height;
+    size_t left, right;
 
-	if (tree == NULL)
-		return (0);
+    if (tree == NULL)
+        return (0);
 
-	left_height = binary_tree_height(tree->left);
-	right_height = binary_tree_height(tree->right);
+    left = binary_tree_height(tree->left);
+    right = binary_tree_height(tree->right);
 
-	if (left_height > right_height)
-		return (left_height + 1);
-
-	return (right_height + 1);
+    return (left > right ? left : right) + (tree->left || tree->right);
 }
