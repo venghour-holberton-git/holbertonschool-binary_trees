0499d3216ab2450470404dde8f52fba1a90e6d8a
venghour-holberton-git <13067@holbertonstudent.com>
2026-04-20 10:45:33 +0000

    fix

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -2,6 +2,7 @@
 
 /**
  * binary_tree_height - correct node-based height
+ * Return: tree height
  */
 size_t binary_tree_height(const binary_tree_t *tree)
 {
@@ -13,11 +14,12 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	left = binary_tree_height(tree->left);
 	right = binary_tree_height(tree->right);
 
-	return (left > right ? left : right) + 1;
+	return ((left > right ? left : right) + 1);
 }
 
 /**
  * binary_tree_balance - measures balance factor
+ * Return: factor
  */
 int binary_tree_balance(const binary_tree_t *tree)
 {
